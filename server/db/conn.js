afccc2ec05f153a48acad4b8050e3d28159d8daa
TIGRAN06 <tigran.nersissian@etu.unice.fr>
2022-01-22 04:54:02 +0100

    Add files via upload

---
+++ server/db/conn.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb');
+const dataBaseName="ProgServer"
+const collectionName="prixEssence"
+const urlINSTANTANE='https://donnees.roulez-eco.fr/opendata/instantane'
+const connectionString = "mongodb://localhost:27017/"
+
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db(dataBaseName);
+      console.log('Successfully connected to MongoDB.');
+
+      return callback();
+    });
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
