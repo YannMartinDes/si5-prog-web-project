1b54f2821dbb0f0f6b58220c623d8731226c6d0a
Mentra20 <yann.martindes@orange.fr>
2022-01-31 16:33:34 +0100

    change prototype for loading marker

--- web/packages/frontend/src/app/app.tsx
+++ web/packages/frontend/src/app/app.tsx
@@ -4,6 +4,16 @@ import styles from './app.module.scss';
 import { Route, Link } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export function App() {
   return (
@@ -25,7 +35,11 @@ export function App() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-
+              <Marker position={[46.25000, 5.64400]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
             </MapContainer>
           </div>
         )}
