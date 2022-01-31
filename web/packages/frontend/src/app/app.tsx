7b34ee8b164bed2b17d73294b7fddb9fe7198c18
Yohann <tognetti.yohann@outlook.fr>
2022-01-31 16:25:06 +0100

    map fonctionnel

--- web/packages/frontend/src/app/app.tsx
+++ web/packages/frontend/src/app/app.tsx
@@ -3,9 +3,7 @@ import styles from './app.module.scss';
 
 import { Route, Link } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { LatLng } from 'leaflet';
 
-const position = new LatLng(51.505, -0.09)
 
 export function App() {
   return (
@@ -21,17 +19,15 @@ export function App() {
         path="/"
         exact
         render={() => (
-          <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
+          <div id='map'>
+            <MapContainer center={[46.25000, 5.64400]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+            </MapContainer>
+          </div>
         )}
       />
     </>
