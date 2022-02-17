218a289f7f5b91acd8edfce7701d0f4cb666f4f1
Quentin <quentin.larose@etu.unice.fr>
2022-02-17 09:28:11 +0100

    Setting login page as home page for dev purposes
    Setting login page as home page for dev purposes

--- web/packages/frontend/src/main.tsx
+++ web/packages/frontend/src/main.tsx
@@ -3,13 +3,14 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+import LoginPage from './app/loginPage'
 import 'leaflet/dist/leaflet.css';
 
 
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <LoginPage></LoginPage>
     </BrowserRouter>
   </StrictMode>,
   document.getElementById('root')
