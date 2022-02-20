419f4696be435c424da04c303b551c4e032c0caa
Yohann <tognetti.yohann@outlook.fr>
2022-02-20 11:39:01 +0100

    evolution css

--- web/packages/frontend/src/app/components/FilterBar.tsx
+++ web/packages/frontend/src/app/components/FilterBar.tsx
@@ -10,37 +10,37 @@ import Button from 'react-bootstrap/esm/Button';
 
 
 export default function FilterBar() {
-  const {dispatch} = useContext(FilterStationContext)
-  const [serviceList,setServiceList]=useState([])
-  const [fuelList,setFuelList]=useState([])
-  const [cityList,setCityList]=useState([])
-  const [hide,setHide] = useState(true);
+  const { dispatch } = useContext(FilterStationContext)
+  const [serviceList, setServiceList] = useState([])
+  const [fuelList, setFuelList] = useState([])
+  const [cityList, setCityList] = useState([])
+  const [hide, setHide] = useState(true);
 
-  useEffect(()=>{
-    axios.get(BACKEND_BASE_URL+"/station/service-type").then((response)=>setServiceList(response.data))
-    axios.get(BACKEND_BASE_URL+"/station/fuel-type").then((response)=>setFuelList(response.data))
-    axios.get(BACKEND_BASE_URL+"/station/city").then((response)=>setCityList(response.data))
+  useEffect(() => {
+    axios.get(BACKEND_BASE_URL + "/station/service-type").then((response) => setServiceList(response.data))
+    axios.get(BACKEND_BASE_URL + "/station/fuel-type").then((response) => setFuelList(response.data))
+    axios.get(BACKEND_BASE_URL + "/station/city").then((response) => setCityList(response.data))
 
-  },[])
+  }, [])
 
   const onCheckBoxChangeGaz = (value: string, checked: boolean) => {
-    if(checked){
-      dispatch({type:ADD_GAS_FILTER,payload:value});
+    if (checked) {
+      dispatch({ type: ADD_GAS_FILTER, payload: value });
     }
-    else{
-      dispatch({type:REMOVE_GAS_FILTER,payload:value});
+    else {
+      dispatch({ type: REMOVE_GAS_FILTER, payload: value });
     }
   }
   const onCheckBoxChangeService = (value: string, checked: boolean) => {
-    if(checked){
-      dispatch({type:ADD_SERVICE_FILTER,payload:value});
+    if (checked) {
+      dispatch({ type: ADD_SERVICE_FILTER, payload: value });
     }
-    else{
-      dispatch({type:REMOVE_SERVICE_FILTER,payload:value});
-    }  
+    else {
+      dispatch({ type: REMOVE_SERVICE_FILTER, payload: value });
+    }
   }
 
-  const onCityChange = (citySelected:any)=>{
+  const onCityChange = (citySelected: any) => {
     console.log(citySelected)
   }
 
@@ -48,33 +48,30 @@ export default function FilterBar() {
     setHide(!hide);
   }
 
-  const test :any = MenuList
+  const test: any = MenuList
+  const filterBarContainer = (
+    <div className="filter-bar-container">
+      <h2>Filtre</h2>
+      <div className='subFilter'>
+        <h3>Ville</h3>
+        <Select components={{ MenuList: test, Option: OptionMenuList }} options={cityList.map((elt) => { return { label: elt, value: elt } })} onChange={onCityChange} ></Select>
+      </div>
+      <div className='subFilter'>
+        <h3>Essences</h3>
+        <CheckBoxList elementList={fuelList} onCheckBoxChange={onCheckBoxChangeGaz}></CheckBoxList>
+      </div>
+      <div className='subFilter'>
+        <h3>Services</h3>
+        <CheckBoxList elementList={serviceList} onCheckBoxChange={onCheckBoxChangeService}></CheckBoxList>
+      </div>
+      <button onClick={(e) => onHideShowClick()}>Hide</button>
+    </div>
+  )
 
-  if(hide){
-    return (
-      <div className='filterBar'>
-        <Button onClick={(e)=> onHideShowClick()}>Show</Button>
-      </div>
-    )
-  }
-  else{
-    return (
-      <div className='filterBar'>
-        <h2>Filtre</h2>
-        <div className='subFilter'>
-          <h3>Ville</h3>
-          <Select components={{MenuList:test,Option:OptionMenuList}} options={cityList.map((elt)=>{return {label:elt,value:elt}})} onChange={onCityChange} ></Select>
-        </div>
-        <div className='subFilter'>
-          <h3>Essences</h3>
-          <CheckBoxList elementList={fuelList} onCheckBoxChange={onCheckBoxChangeGaz}></CheckBoxList>
-        </div>
-        <div className='subFilter'>
-          <h3>Services</h3>
-          <CheckBoxList elementList={serviceList} onCheckBoxChange={onCheckBoxChangeService}></CheckBoxList>
-        </div>
-        <button onClick={(e)=> onHideShowClick()}>Hide</button>
-      </div>
-    )
-  }
+
+  return (
+    <div className='filterBar'>
+      {hide? <Button onClick={(e) => onHideShowClick()}>Show</Button> : filterBarContainer}
+    </div>
+  )
 }
