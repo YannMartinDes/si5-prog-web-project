7c8062e9941f3bbc859cda136dc0427a6ea82268
Yohann <tognetti.yohann@outlook.fr>
2022-02-21 20:54:22 +0100

    amelioration position marker icon

--- web/packages/frontend/src/assets/GasStationIcon.ts
+++ web/packages/frontend/src/assets/GasStationIcon.ts
@@ -4,8 +4,8 @@ var gasStationIcon = L.icon({
         iconUrl: '../assets/gasMarker.png',
         
         iconSize:     [60, 60], // size of the icon
-        iconAnchor:   [18, 14], // point of the icon which will correspond to marker's location
-        popupAnchor:  [13, -5] // point from which the popup should open relative to the iconAnchor
+        iconAnchor:   [30, 54], // point of the icon which will correspond to marker's location
+        popupAnchor:  [30, 54] // point from which the popup should open relative to the iconAnchor
 });
 
 export default gasStationIcon;
