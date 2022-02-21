e35d1385a0c7142646e3c7387e1effcecd923361
TIGRAN06 <tigran.nersissian@etu.unice.fr>
2022-02-21 20:43:51 +0100

    deplace zoom control bottom right

--- web/packages/frontend/src/app/components/GlobalMap.tsx
+++ web/packages/frontend/src/app/components/GlobalMap.tsx
@@ -1,28 +1,29 @@
 import "./GlobalMap.scss"
 import { GasStationPosition, Position } from '@web/common/dto';
 import { useContext, useState } from 'react';
-import {MapContainer, TileLayer, useMap} from 'react-leaflet';
+import {MapContainer, TileLayer, useMap, ZoomControl} from 'react-leaflet';
 import MapMarker from './MapMarker';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import { GeolocalisationContext } from "../context/GeolocalisationContext";
 import PositionUpdater from "./PositionUpdater";
 import { MapContext } from "../context/MapContext";
-import { Map } from "leaflet";
+import { control, Map } from "leaflet";
 
 export default function GlobalMap({markersList}:
 {markersList:GasStationPosition[]})
 {
   const {position} = useContext(GeolocalisationContext)
-  const [map,setMap]:[Map,any] = useContext(MapContext);
   
+  const [map,setMap]:[Map,any] = useContext(MapContext);
     return(
         <div id='map' className='mapDisplayer'>
-            <MapContainer center={[position.lat,position.lon]} zoom={13} scrollWheelZoom={false} whenCreated={setMap}>
+            <MapContainer center={[position.lat,position.lon]} zoom={13} zoomControl = {false} scrollWheelZoom={false} whenCreated={setMap}>
               <PositionUpdater />
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <ZoomControl position = "bottomright"/>
                 <MarkerClusterGroup >
                   {markersList.map((value,index) => {return (<MapMarker key={value.id} gasStation={value} />)})}
                 </MarkerClusterGroup>
