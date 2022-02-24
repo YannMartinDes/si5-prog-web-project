d28bfa181578374af5298855eb05df636c048269
Mentra20 <yann.martindes@orange.fr>
2022-02-24 17:34:01 +0100

    sort in station List

--- web/packages/frontend/src/app/components/left-menu/StationList.tsx
+++ web/packages/frontend/src/app/components/left-menu/StationList.tsx
@@ -1,19 +1,47 @@
 import "./StationList.scss"
 import { GasStationPosition } from '@web/common/dto'
-import React from 'react'
+import React, { useState } from 'react'
 import StationListElement from './StationListElement'
 import { TailSpin } from "react-loader-spinner"
+import { stat } from "fs"
 
 export default function StationList({stationList}:
     {stationList:GasStationPosition[]}) {
+    
+  const [isAscending, setIsAscending] = useState(true);
+
+
+  const sortAscending = (a:GasStationPosition,b:GasStationPosition)=>{
+    if(a.address < b.address) return -1;
+    if(a.address > b.address) return 1;
+    return 0; 
+  }
+
+  const sortDescending = (a:GasStationPosition,b:GasStationPosition)=>{
+    if(a.address > b.address) return -1;
+    if(a.address < b.address) return 1;
+    return 0; 
+  }
+
+  const sortListClick = ()=>{
+    let sortMethod = null;
+    stationList.sort((a,b)=> {
+      isAscending? sortMethod = sortAscending: sortMethod = sortDescending
+      return sortMethod(a,b);
+    })
+    setIsAscending(!isAscending);
+  }
 
   return (
     <div className='stationList'>
       <h1>Stations Essences :</h1>
       {(stationList.length) !== 0 ?
-        stationList.map((station)=>{
-          return <StationListElement gasStation={station}/>
-        }) 
+        (<div>
+          <button className="buttonStyle" onClick={sortListClick}>Trier A-Z</button>
+          {stationList.map((station)=>{
+            return <StationListElement gasStation={station}/>
+          })}
+        </div>)
       :(
         <div>
           <h2>Chargement...</h2>
