e8c6a873bc8eb4d2b1641e85901222cde138dfae
Yohann <tognetti.yohann@outlook.fr>
2022-02-24 12:02:42 +0100

    #23 darktheme switch + correction

--- web/packages/frontend/src/app/components/NavBar.tsx
+++ web/packages/frontend/src/app/components/NavBar.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigateNoUpdates } from '../context/RouterUtils';
-import FilterBar from './FilterBar';
+import { ThemeContext } from '../context/ThemeContext';
 import "./NavBar.scss"
+import { Form } from 'react-bootstrap';
+
 
 export default function NavBar() {
 
     const [hideBar, setHideBar] = useState(true);
     const navigate = useNavigateNoUpdates();
+    const {isDarkTheme,setDarkTheme} = useContext(ThemeContext)
 
     const onHideShowClick = () => {
         setHideBar(!hideBar);
         console.log("hello")
     }
+
 
     return (
         <div className=''>
@@ -20,8 +24,13 @@ export default function NavBar() {
                 <button className="buttonStyle" onClick={(e) => navigate(`chart`)}>Graphiques</button>
                 <button className="buttonStyle filterButton" onClick={(e) => onHideShowClick()}>{hideBar? "Afficher Filtre" : "Masquer Filtre"}</button>
                 <button className="loginButton buttonStyle" onClick={(e) => navigate(`login`)}>Profil Utilisateur</button>
+                <Form.Check 
+                    type="switch"
+                    className='custom-theme-swtich'
+                    checked={isDarkTheme}
+                    onChange={event=>setDarkTheme(event.target.checked)}
+                />
           </div>
-          {!hideBar && <FilterBar />}
         </div> 
     )
 }
--- web/packages/frontend/src/app/context/ThemeContext.jsx
+++ web/packages/frontend/src/app/context/ThemeContext.jsx
@@ -1,13 +1,40 @@
-import { createContext, useState} from 'react';
+import { createContext, useState,useEffect} from 'react';
 import PropTypes from 'prop-types';
 
 export const ThemeContext = createContext();
 
 export const ThemeContextProvider = ({ children }) => {
-    const [isDarkTheme,setDarkTheme] = useState(true)
+	const [isDarkTheme,setDarkTheme] = useState(false)
+
+	// const [isDark] = useDarkreader()
+
+	useEffect(() => {
+		if(localStorage){
+			let darkTheme =false;
+			const darkThemeStr = localStorage.getItem("dark-theme")
+			if(darkThemeStr!==null){
+				darkTheme = Boolean(darkThemeStr)
+			}
+			setDarkTheme(Boolean(darkTheme))
+		}
+	}, [setDarkTheme])
+
+	useEffect(() => {
+		if(localStorage){
+			localStorage.setItem("dark-theme",isDarkTheme)
+		}
+		if(isDarkTheme){
+			document.body.classList.add("dark-body")
+		}else{
+			document.body.classList.remove("dark-body")
+
+		}
+	}, [isDarkTheme])
+	
+	console.log("hello" +isDarkTheme)
 	return (
 		<ThemeContext.Provider value={{isDarkTheme, setDarkTheme}} >
-            <div className={`app-container ${isDarkTheme?"theme--dark":"theme--default"}`}>
+            <div className={`app-container ${(isDarkTheme)?"theme--dark":"theme--default"}`}>
 			    {children}
             </div>
 		</ThemeContext.Provider>
