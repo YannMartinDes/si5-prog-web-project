0c10a6c90a1c8d1b9c7c086a1697343fcf2b0a8d
Yohann <tognetti.yohann@outlook.fr>
2022-02-25 00:16:55 +0100

    #23 impossible d'avoir beaucoup de tail avec la map en darkmode

--- web/packages/frontend/src/app/components/map/GlobalMap.tsx
+++ web/packages/frontend/src/app/components/map/GlobalMap.tsx
@@ -14,34 +14,19 @@ import PositionMarker from "./PositionMarker";
 
 
 
-const themeDark = L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",{attribution:'&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'})
-const themeLight = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
-
 export default function GlobalMap({markersList}:
 {markersList:GasStationPosition[]})
 {
-  const {isDarkTheme} = useContext(ThemeContext)
   const [position] = useContext(GeolocalisationContext)
   const [map,setMap]:[Map,any] = useContext(MapContext);
-
   
-  useEffect(()=>{
-    if(map){
-      map.eachLayer((layer:any)=>{
-        if(layer._url){
-          map.removeLayer(layer)
-        }
-      }
-        )
-      map.addLayer(isDarkTheme?themeDark:themeLight)
-    }
-  },[isDarkTheme, map])
+  
 
     return(
         <div id='map' className='mapDisplayer'>
             <MapContainer center={[position.lat,position.lon]} zoom={13} zoomControl = {false} scrollWheelZoom={false} whenCreated={setMap}>
               <PositionUpdater />
-              <TileLayer attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
+              <TileLayer className='map-tiles' attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
                 url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
                 />
               <ZoomControl position = "bottomright"/>
