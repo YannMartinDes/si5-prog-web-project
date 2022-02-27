0e1039281cb80ffcc74ae9009a130ee222d40c8f
Quentin <quentin.larose@etu.unice.fr>
2022-02-27 03:34:11 +0100

    Adding new component to display favorite stations
    Adding new component to display favorite stations

---
+++ web/packages/frontend/src/app/components/favorite-stations/FavStationMenu.tsx
@@ -0,0 +1,19 @@
+import "./LeftSideMenu.scss"
+import { GasStationPosition } from '@web/common/dto'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { FRONT_STATION_ID } from '../../const/url.const'
+import StationList from "../left-menu/StationList";
+import StationDetailed from "../left-menu/StationDetailed";
+
+export default function FavStationMenu({gasStationList}:{gasStationList:GasStationPosition[]}) {
+  return (
+    <div className='sideMenu'>
+      STATIONS FAVORITES
+      <Routes>
+        <Route path="/" element={<StationList stationList={gasStationList}/>}/>
+        <Route path={FRONT_STATION_ID+":id"} element={<StationDetailed/>}/>
+      </Routes>
+    </div>
+  )
+}
