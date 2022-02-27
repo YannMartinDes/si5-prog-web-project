2fb65d0d342c25ff40b9dd080bca1d3a1a7ef54b
Mentra20 <yann.martindes@orange.fr>
2022-02-27 14:18:23 +0100

    button change if filter is open

--- web/packages/frontend/src/app/components/nav-bar/NavBar.tsx
+++ web/packages/frontend/src/app/components/nav-bar/NavBar.tsx
@@ -31,10 +31,10 @@ export default function NavBar() {
             <div className='navBar'>
                 <button className="buttonStyle" onClick={(e) => navigate(`/`)}>Accueil</button>
                 <button className="buttonStyle" onClick={(e) => navigate(`chart`)}>Graphiques</button>
-                <button className="buttonStyle filterButton" onClick={(e) => onHideShowClick()}>{hideBar? "Afficher Filtre" : "Masquer Filtre"}</button>
+                <button className={hideBar? "buttonStyle" :"buttonStyleRed"} onClick={(e) => onHideShowClick()}>{hideBar? "Afficher Filtre" : "Masquer Filtre"}</button>
                 <button className="loginButton buttonStyle" onClick={(e) => navigate(`login`)}>{buttonValue}</button>
                 {!hideBar && <FilterBar />}
-                <Switch onHandleColor="#444" offHandleColor="#0A5C72" offColor="#333" onColor="#ccc" activeBoxShadow="0 0 2px 2px rgba(150,150,150,0.25)" className="custom-theme-swtich" onChange={onChangeTheme} checked={isDarkTheme}/>
+                <Switch onHandleColor="#444" offHandleColor="#0A5C72" offColor="#333" onColor="#ccc" activeBoxShadow="0 0 2px 2px rgba(150,150,150,0.25)" className="custom-theme-switch" onChange={onChangeTheme} checked={isDarkTheme}/>
             </div>
         </div>
     )
