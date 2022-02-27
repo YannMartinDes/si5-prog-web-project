8f01fc2d3aca27a379286c5797d5367fc11a7595
Quentin <quentin.larose@etu.unice.fr>
2022-02-27 03:39:01 +0100

    #26 Feature : Add station to favorite stations (in station details)
    #26 Feature : Add station to favorite stations (in station details)

--- web/packages/frontend/src/app/components/left-menu/StationDetailed.tsx
+++ web/packages/frontend/src/app/components/left-menu/StationDetailed.tsx
@@ -1,16 +1,18 @@
 import "./StationDetailed.scss"
 import { GasStationInfo, UserIssue } from '@web/common/dto';
-import React, { useEffect, useState } from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { BACKEND_BASE_URL, REPORT_ISSUE, STATION_INFO } from '../../const/url.const';
+import {BACKEND_BASE_URL, REPORT_ISSUE, STATION_INFO, UPDATE_FAVORITE_STATION_URL} from '../../const/url.const';
 import {TailSpin} from 'react-loader-spinner'
 import { useNavigateNoUpdates } from "../../context/RouterUtils";
+import {AuthContext} from "../../context/AuthContext";
 
 export default function SideMenu() {
   const navigate = useNavigateNoUpdates()
   const [gasStationInfo,setGasStationInfo] = useState<GasStationInfo>();
   const {id} = useParams();
+  const {token, user, favoriteStations, setFavoriteStations} = useContext(AuthContext)
 
   function getStationInfo(stationId:string) {
     console.log("GET STATION INFO FOR "+stationId)
@@ -50,11 +52,55 @@ export default function SideMenu() {
       console.error("NO ID FOUND");
   },[id])
 
+  /**
+   * To add the current station to the favorite stations of the user
+   */
+  function addFavoriteStation(){
+    if(user!=''){
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      console.log("entering favorites : " , (!favoriteStations.some((e: GasStationInfo) => e.id === gasStationInfo.id)));
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      //if the array already contains this favorite, then we do not add it
+      if(!favoriteStations.some((e: GasStationInfo) => e.id === gasStationInfo.id)){
+        const newFavoriteStations = favoriteStations;
+        console.log(newFavoriteStations);
+        newFavoriteStations.push(gasStationInfo);
+        setFavoriteStations(newFavoriteStations);
+        setNewFavorite().then(
+          res => {
+            console.log('Favorites updated for current user !')
+          }
+        );
+      } else {
+        console.log('Already added to favorites !');
+      }
+    }
+    console.log('lets see current favorites : ', favoriteStations);
+  }
+
+  async function setNewFavorite(){
+    if(user!=''){
+      console.log("CALL BACKEND FOR FAVORITE STATION OF ", user);
+      try {
+        console.log('route used : ', BACKEND_BASE_URL + UPDATE_FAVORITE_STATION_URL);
+        const favorite = await axios.post(BACKEND_BASE_URL + UPDATE_FAVORITE_STATION_URL,
+          {headers: {Authorization: 'Bearer ' + token},
+            body:    {favoriteStations}
+          });
+        console.log('favorite stations updated !');
+      } catch (e){
+        console.log(e);
+      }
+    }
+  }
+
   return(
     <div className='stationDetailed'>
         <h1>{gasStationInfo?.address||"chargement..."}</h1>
         <h3>{gasStationInfo?.id||"chargement..."}</h3>
-  
+
         <div className='subInfo'>
           <h2>Essences</h2>
           {gasStationInfo? gasStationInfo.prices.map((value) => {
@@ -63,7 +109,7 @@ export default function SideMenu() {
           }):
           <TailSpin color="#063d44" width={60} height={60}/>}
         </div>
-      
+
         <div className='subInfo'>
           <h2>Services disponibles</h2>
           <ul>
@@ -74,9 +120,9 @@ export default function SideMenu() {
 
         <div className='subInfo'>
           <h2>Horaire</h2>
-          
-            {gasStationInfo? 
-              ((gasStationInfo.schedules.length !== 0) ? 
+
+            {gasStationInfo?
+              ((gasStationInfo.schedules.length !== 0) ?
                 (<ul>
                   {gasStationInfo.schedules.map((schedule) => {
                     const scheduleText = schedule.day + (schedule.openned? " ouvert ":" fermé ");
@@ -92,6 +138,7 @@ export default function SideMenu() {
                 : (<p>Pas d'informations disponibles</p>))
             :<TailSpin color="#063d44" width={60} height={60}/>}
         </div>
+        <button className='buttonStyle' onClick={(e)=>{addFavoriteStation()}}>Ajouter la station aux favoris</button>
         <button className='buttonStyle' onClick={(e)=>{onBackClick()}} >{"<< Liste des stations"}</button>
         <button className='buttonStyleRed' onClick={(e)=>{onUserReportClick(gasStationInfo?.id!)}} >Signaler un problème</button>
     </div>
